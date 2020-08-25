15179d4b582a299b6c70f5f94315448d4640ea4b
nikhilbhide <nikhilbhide15@gmail.com>
2020-08-25 19:32:31 +0530

    changed implementation to handle reactor can not be restarted issue

--- scrapy-serverless/scraper/quote_scraper/crawl.py
+++ scrapy-serverless/scraper/quote_scraper/crawl.py
@@ -3,17 +3,31 @@ import imp
 import os
 import logging
 from urllib.parse import urlparse
-
 from scrapy.spiderloader import SpiderLoader
+from scrapy import crawler
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerRunner
+from importlib import import_module 
+from multiprocessing import Process, Queue
+from twisted.internet import reactor
 
 # Need to "mock" sqlite for the process to not crash in AWS Lambda / Amazon Linux
 sys.modules["sqlite"] = imp.new_module("sqlite")
 sys.modules["sqlite3.dbapi2"] = imp.new_module("sqlite.dbapi2")
+result = None
+
+def spider_closing(spider):
+    """Activates on spider closed signal"""
+    logging.msg("Spider closed: %s" % spider, level=log.INFO)
+    reactor.stop()
+    logging.msg("reactor stopping")
 
 def is_in_aws():
     return os.getenv('AWS_EXECUTION_ENV') is not None
+
+def set_result(item):
+    result = item
 
 def crawl(settings={}, spider_name="quote_spider", spider_kwargs={}):
     project_settings = get_project_settings()
@@ -24,12 +38,36 @@ def crawl(settings={}, spider_name="quote_spider", spider_kwargs={}):
     feed_uri = ""
     feed_format = "json"
 
-    # Lambda can only write to the /tmp folder.
-    settings['HTTPCACHE_DIR'] =  "/tmp"
-    settings['FEED_URI'] = feed_uri
-    settings['FEED_FORMAT'] = feed_format
+    if is_in_aws():
+		# Lambda can only write to the /tmp folder.
+        settings['HTTPCACHE_DIR'] =  "/tmp"
 
     process = CrawlerProcess({**project_settings, **settings})
 
     process.crawl(spider_cls, **spider_kwargs)
-    process.start()
+    process.start() 
+
+    try:
+        reactor.stop()    
+    except Exception as e:
+        print(e)
+# # the wrapper to make it run more times
+def run_spider(spider):
+    def f(q):
+        try:
+            runner = crawler.CrawlerRunner()
+            deferred = runner.crawl(spider)
+            deferred.addBoth(lambda _: reactor.stop())
+            reactor.run()
+            q.put(None)
+        except Exception as e:
+            q.put(e)
+
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    result = q.get()
+    p.join()
+
+    if result is not None:
+        raise result     
--- scrapy-serverless/scraper/quote_scraper/spiders/quote_spider.py
+++ scrapy-serverless/scraper/quote_scraper/spiders/quote_spider.py
@@ -1,5 +1,10 @@
 import scrapy
 from ..items import QuoteScraperItem
+from scrapy import signals
+import scrapy
+import scrapy.crawler as crawler
+from multiprocessing import Process, Queue
+from twisted.internet import reactor
 
 class QuoteSpider(scrapy.Spider):
     name = "quote_spider"
@@ -7,16 +12,55 @@ class QuoteSpider(scrapy.Spider):
         "http://quotes.toscrape.com/"
     ]
 
-    def parse(self, response):
-        quotes = response.css(".quote")
-        items = QuoteScraperItem()
-        for quote in quotes:
-            quote_text = quote.css(".text::text").extract()
-            author = quote.css(".author::text").extract()
-            tags = quote.css(".tag::text").extract()
-  
-            items['quote_text'] = quote_text
-            items['author'] = author
-            items['tags'] = tags
-           
-            yield items
+    def parse(self,response):
+        # The main method of the spider. It scrapes the URL(s) specified in the
+        # 'start_url' argument above. The content of the scraped URL is passed on
+        # as the 'response' object.
+
+        nextpageurl = response.css(".next a ::attr(href)")
+        
+        # When asked for a new item, ask self.scrape for new items and pass them along
+        yield from self.scrape(response)
+    
+        if nextpageurl:
+            path = nextpageurl.extract_first()
+            nextpage = response.urljoin(path)
+            print("Found url: {}".format(nextpage))
+            yield scrapy.Request(nextpage, callback=self.parse)
+
+    def scrape(self, response):
+      quotes = response.css(".quote")
+      items = QuoteScraperItem()
+      for quote in quotes:
+        quote_text = quote.css(".text::text").extract()
+        author = quote.css(".author::text").extract()
+        tags = quote.css(".tag::text").extract()
+
+        items['quote_text'] = quote_text
+        items['author'] = author
+        items['tags'] = tags
+        
+        yield items
+
+    # the wrapper to make it run more times
+    def run_spider(spider):
+        def f(q):
+            try:
+                runner = crawler.CrawlerRunner()
+                deferred = runner.crawl(spider)
+                deferred.addBoth(lambda _: reactor.stop())
+                reactor.run()
+                q.put(None)
+            except Exception as e:
+                q.put(e)
+
+        q = Queue()
+        p = Process(target=f, args=(q,))
+        p.start()
+        result = q.get()
+        p.join()
+
+        if result is not None:
+            raise result
+            
+                
