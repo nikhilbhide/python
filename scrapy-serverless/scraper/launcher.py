7f89451d6d656130d14bf3e86409966d3096da21
nikhilbhide <nikhilbhide15@gmail.com>
2020-08-28 19:52:08 +0530

    refactored implementation for scraper

--- scrapy-serverless/scraper/launcher.py
+++ scrapy-serverless/scraper/launcher.py
@@ -1,10 +1,13 @@
 import sys
 import json
-
-from quote_scraper.crawl import crawl
+from quote_scraper.spiders.quote_spider import QuoteSpider
+from quote_scraper.crawl import crawl, run_spider
+import boto3 
+from rest.get import get_by_author_name
 
 def scrape(event={}, context={}):
     crawl(**event)
+    #run_spider(QuoteSpider)
 
 if __name__ == "__main__":
     try:
